6b59c796da8fda3350673de3ecc48ddef9a2c2ac
SeoDongHee <seoddong@hanmail.net>
2016-04-25 23:06:45 +0900

    Cell들이 사진앱과 비슷한 UX를 제공하도록 설정

--- BurstAnimator/BurstAlbumVC.swift
+++ BurstAnimator/BurstAlbumVC.swift
@@ -64,6 +64,55 @@ class BurstAlbumVC: UIViewController, UICollectionViewDelegate, UICollectionView
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    // Collection View가 처리되는 과정은 아래 메소드의 순서대로이다.
+    // 1. numberOfSectionsInCollectionView가 가정 먼저 실행되어 Section의 개수를 파악한다.
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        
+        // 이 샘플에서는 Section은 1개이다.
+        print("numberOfSectionsInCollectionView = 1")
+        return 1
+    }
+    
+    // 2. numberOfItemsInSection가 실행되어 Section당 Item의 개수를 파악한다.
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        print("numberOfItemsInSection = \(burstImages.count)")
+        // 이 샘플에서는 images의 개수가 cell의 개수이다.
+        return burstImages.count
+    }
+    
+    // 3. 셀의 크기 설정이 이루어진다.
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        // 사진앱과 가장 비슷한 UX를 제공하려면 minimumInteritemSpacingForSectionAtIndex, minimumLineSpacingForSectionAtIndex 둘 다 1로 설정하는 것이다.
+        // 이 크기를 감안해서 Cell의 크기를 설정해 주어야 한다.
+        // 만약 Spacing을 고려하지 않고 Cell 크기를 설정하게 되어 미묘하게 Cell 크기가 가로 크기를 넘길 경우 이쁘지 않은 레이아웃을 보게 될 것이다.
+        // 그러므로 최종 Cell의 크기는 Spacing 값을 참조하여 빼주도록 한다.
+        targetSizeX = burstAlbumCollectionView.frame.width / 3 - 1 // Min Spacing For Cell
+        print("Cell 크기 설정 - targetSizeX = \(targetSizeX)")
+        
+        return CGSizeMake(targetSizeX, targetSizeX)
+    }
+    
+    
+    // 4. Cell 내부 아이템의 최소 스페이싱을 설정한다. 셀간의 가로 간격이라고 생각하면 된다.
+    // 상세한 내역은 여기 참조 : https://developer.apple.com/library/ios/documentation/WindowsViews/Conceptual/CollectionViewPGforIOS/UsingtheFlowLayout/UsingtheFlowLayout.html
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        
+        print("minimumInteritemSpacingForSectionAtIndex 설정")
+        return 1 as CGFloat
+    }
+    
+    // 5. Cell 간 라인 스페이싱을 설정한다. 셀간의 세로 간격이라고 생각하면 된다.
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        
+        print("minimumLineSpacingForSectionAtIndex 설정")
+        return 1 as CGFloat
+    }
+    
+    // 셀의 내용을 설정하는 메소드
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as? BurstAlbumCVC {
@@ -80,46 +129,11 @@ class BurstAlbumVC: UIViewController, UICollectionViewDelegate, UICollectionView
         }
     }
     
-    
+    // 셀이 선택되었을 때를 설정하는 메소드
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         
         // 나중에 정의
     }
-    
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        // 이 샘플에서는 images의 개수가 cell의 개수이다.
-        return burstImages.count
-    }
-    
-    
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        
-        // 이 샘플에서는 Section은 1개이다.
-        return 1
-    }
-    
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        
-        // Cell spacing과 padding값에 의해서 셀 크기가 계산되어졌다 하더라도 제대로 안 들어가는 경우가 있다.
-        // Cell spacing은 10정도 유지하는 것이 좋고 padding은 없는 것이 사진앱과 비슷한 UX를 제공한다.
-        // 그러므로 최종 Cell의 크기는 CollectionView의 Min Spacing For Cell 값을 참조하여 빼주도록 한다.
-        targetSizeX = burstAlbumCollectionView.frame.width / 3 - 1 // Min Spacing For Cell
-        print("Cell 크기 설정 - targetSizeX = \(targetSizeX)")
-        
-        return CGSizeMake(targetSizeX, targetSizeX)
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
-        return 0 as CGFloat
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
-        return 1 as CGFloat
-    }
-    
     
     /*
     // MARK: - Navigation
