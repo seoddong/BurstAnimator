a42cc0cef7c430384709fdd237d134968286a043
SeoDongHee <seoddong@hanmail.net>
2016-05-01 23:09:05 +0900

    BurstImages를 가져와서 Animating 할 때 dispatch_async를 이용해보았는데, 잘 안 된다. 실패다.

--- BurstAnimator/AnimateVC.swift
+++ BurstAnimator/AnimateVC.swift
@@ -19,7 +19,7 @@ class AnimateVC: UIViewController {
     // 아래와 같이 optional 빼고 초기값 주었더니 오류는 안 난다.
     var animatedimagesarray: [UIImage] = []
     
-    let imagemanager = PHCachingImageManager()
+    let imagemanager = PHCachingImageManager.defaultManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,8 +41,8 @@ class AnimateVC: UIViewController {
         // iCloud 이미지를 가져올 수도 있으므로 네트워크 사용을 허용한다.
         option.networkAccessAllowed = true
         option.deliveryMode = .HighQualityFormat
-        // option.synchronous = false로 하게 되면 사진을 다 가져오지 못 한 상태에서 다음 루프문이 실행되기 때문에 nil이 생기고 에러가 발생한다.
-        option.synchronous = true
+        // option.synchronous = false로 하게 되면 사진을 다 가져오지 못 한 상태에서 다음 루프문이 실행되기 때문에 nil이 생기고 에러가 발생한다. 그러므로 사진을 가져오는 부분에서 dispatch_async를 써줘야 한다.
+        option.synchronous = false
         
         // progress bar를 넣을 예정 - 각각의 이미지 로딩 진척률을 조합하여 계산 필요.
         
@@ -53,19 +53,31 @@ class AnimateVC: UIViewController {
             
             // 도대체 왜 이미지가 찌그러지는지 알 수가 없다.
             self.imagemanager.requestImageForAsset(imageasset, targetSize: size, contentMode: .AspectFit, options: option, resultHandler: { (result, info) -> Void in
-                self.animatedimagesarray.append(result!)
+                
+                // 아래 코드는 option.synchronous = false 일 때 사용한다.
+                dispatch_async(dispatch_get_main_queue(), {() -> Void in
+                    // 가져온 이미지는 화면의 이미지뷰에 세팅해준다.
+                    self.animatedimagesarray.append(result!)
+                })
+                
             })
             
         }
         
-        animatedImageView.animationImages = animatedimagesarray
-        // animationDuration은 animation이 플레이될 총 시간(sec)을 의미한다. 그러므로 이미지 개수에 따라 총 시간을 다르게 설정해주어야 한다.
-        animatedImageView.animationDuration = Double(fetchresult.count) * 0.1
-        animatedImageView.animationRepeatCount = 0
-        animatedImageView.startAnimating()
+        dispatch_async(dispatch_get_main_queue(), {() -> Void in
+            self.animatedImageView.animationImages = self.animatedimagesarray
+            // animationDuration은 animation이 플레이될 총 시간(sec)을 의미한다. 그러므로 이미지 개수에 따라 총 시간을 다르게 설정해주어야 한다.
+            self.animatedImageView.animationDuration = Double(self.fetchresult.count) * 0.1
+            self.animatedImageView.animationRepeatCount = 0
+            print("startAnimating")
+            self.animatedImageView.startAnimating()
+            })
         
         // 아래 코드가 필요할까 잘 모르겠다.
-        self.view.layoutIfNeeded()
+        //self.view.layoutIfNeeded()
+        
+        let size2: CGSize = CGSizeMake(self.animatedImageView.bounds.width, self.animatedImageView.bounds.height)
+        print("size = \(size2)")
     }
 
     override func didReceiveMemoryWarning() {
