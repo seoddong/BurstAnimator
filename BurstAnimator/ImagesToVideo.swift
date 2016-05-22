68a22e88145d2f0f643c4a16e4f2dd75944afc9d
SeoDongHee <seoddong@hanmail.net>
2016-05-23 00:45:56 +0900

    input.requestMediaDataWhenReadyOnQueue를 사용하여 보았으나 실패

--- BurstAnimator/ImagesToVideo.swift
+++ BurstAnimator/ImagesToVideo.swift
@@ -81,138 +81,147 @@ class ImagesToVideo {
         var pixelBufferPointer = UnsafeMutablePointer<CVPixelBuffer?>.alloc(1)
         CVPixelBufferPoolCreatePixelBuffer(nil, adaptor.pixelBufferPool!, pixelBufferPointer)
         
-        // 아래 10은 10fps를 의미한다.
-        let fps: Int32 = 10
+        let assetWriterQueue = dispatch_queue_create("AssetWriterQueue", DISPATCH_QUEUE_SERIAL);
         
-        var ii = 0
-        
-        // 초당 10프레임짜리 중 0번째 프레임을 의미함
-        var presentTime = CMTimeMake(Int64(ii), fps)
-        
-        debugPrint("presentTime = \(presentTime)")
-        var boolWhile = true
-        var readyForMoreMediaDataFalseCount = 0
-        while (boolWhile) {
+        input.requestMediaDataWhenReadyOnQueue(assetWriterQueue) {() -> Void in
+            // 아래 10은 10fps를 의미한다.
+            let fps: Int32 = 10
             
-            // 아래 autoreleasepool 찾는데 한 4일 걸렸다. 이 코드를 사용하지 않으면 memory usage가 계속 올라가서 500메가도 넘다가 결국 앱이 죽고 만다.
-            autoreleasepool({() -> () in
+            var ii = 0
+            
+            // 초당 10프레임짜리 중 0번째 프레임을 의미함
+            var presentTime = CMTimeMake(Int64(ii), fps)
+            
+            debugPrint("presentTime = \(presentTime)")
+            var boolWhile = true
+            var readyForMoreMediaDataFalseCount = 0
+            while (boolWhile) {
                 
-                if(input.readyForMoreMediaData) {
-                    presentTime = CMTimeMake(Int64(ii), fps)
+                // 아래 autoreleasepool 찾는데 한 4일 걸렸다. 이 코드를 사용하지 않으면 memory usage가 계속 올라가서 500메가도 넘다가 결국 앱이 죽고 만다.
+                autoreleasepool({() -> () in
+                    var boolReady = false
+                    boolReady = input.readyForMoreMediaData
                     
-                    if(ii >= arrayImages.count){
+                    if(boolReady) {
+                        presentTime = CMTimeMake(Int64(ii), fps)
                         
-                        // 비디오 변환 후 마지막 이미지의 pxbuffer만 이미지뷰에 표시한다.
-                        let parentVC = sender as! AnimateVC
-                        let checkImage = CIImage.init(CVPixelBuffer: pixelBufferPointer.memory!)
-                        parentVC.animatedImageView.image = UIImage.init(CIImage: checkImage)
-                        
-                        
-                        pixelBufferPointer.dealloc(1)
-                        //pixelBufferPointer.destroy()
-                        pixelBufferPointer = nil
-                        
-                        input.markAsFinished()
-                        videoWriter.endSessionAtSourceTime(presentTime)
-                        videoWriter.finishWritingWithCompletionHandler({ () -> Void in
-                            if videoWriter.status == AVAssetWriterStatus.Failed {
-                                debugPrint("oh noes, an error: \(videoWriter.error!.description)")
-                            } else {
-                                debugPrint("hrmmm, there should be a movie?")
-                                UISaveVideoAtPathToSavedPhotosAlbum(tempPath, self, nil, nil);
-                                debugPrint("saved..")
-                                boolWhile = false
+                        if(ii >= arrayImages.count){
+                            
+                            // 비디오 변환 후 마지막 이미지의 pxbuffer만 이미지뷰에 표시한다.
+                            let parentVC = self.sender as! AnimateVC
+                            let checkImage = CIImage.init(CVPixelBuffer: pixelBufferPointer.memory!)
+                            parentVC.animatedImageView.image = UIImage.init(CIImage: checkImage)
+                            
+                            
+                            pixelBufferPointer.dealloc(1)
+                            //pixelBufferPointer.destroy()
+                            pixelBufferPointer = nil
+                            
+                            input.markAsFinished()
+                            videoWriter.endSessionAtSourceTime(presentTime)
+                            videoWriter.finishWritingWithCompletionHandler({ () -> Void in
+                                if videoWriter.status == AVAssetWriterStatus.Failed {
+                                    debugPrint("oh noes, an error: \(videoWriter.error!.description)")
+                                } else {
+                                    debugPrint("hrmmm, there should be a movie?")
+                                    UISaveVideoAtPathToSavedPhotosAlbum(tempPath, self, nil, nil);
+                                    debugPrint("saved..")
+                                    boolWhile = false
+                                }
+                            })
+                            
+                        }
+                        else {
+                            
+                            debugPrint("ii=\(ii)")
+                            
+                            // The current solution i am using is to separate the writing task to different segments, each segment writes a single file. At the end of the loop, i use AVMutableComposition to combine all small files. By this method, peak memory appears in each segment. Because each segment there are not many imgs to write, the memory will not be consumed too much. I appreciate other ideas
+                            // 아직까지 방법은 이와 같이 파일을 잘게 쪼개서 저장했다가 합치는 것 밖에 없는 것 같다.
+                            pixelBufferPointer = self.pixelBufferFromCGImage(arrayImages[ii] as! UIImage)
+                            adaptor.appendPixelBuffer(pixelBufferPointer.memory!, withPresentationTime: presentTime)
+                            
+                            ii += 1
+                            
+                            while (!boolReady) {
+                                // 초당 10프레임짜리 3초 짜리 동영상 만드는데 여기 5000번 가량 들어온다. 10ms씩 재워주면 1000번 가량으로 줄어든다.
+                                usleep(10);
                             }
-                        })
+                        }
                         
                     }
                     else {
-                        
-                        debugPrint("ii=\(ii)")
-
-                        //pixelBufferPointer = self.pixelBufferFromCGImage(arrayImages[ii] as! CGImage, size: outputSize)
-                        self.pixelBufferFromCGImage(arrayImages[ii] as! UIImage, pxbuffer: pixelBufferPointer)
-                        
-                        
-                        while (!input.readyForMoreMediaData) {
-                            // 초당 10프레임짜리 3초 짜리 동영상 만드는데 여기 5000번 가량 들어온다. 10ms씩 재워주면 1000번 가량으로 줄어든다.
-                            usleep(10);
-                        }
-                        adaptor.appendPixelBuffer(pixelBufferPointer.memory!, withPresentationTime: presentTime)
-                        
-                        ii += 1
+                        readyForMoreMediaDataFalseCount += 1
+                        //debugPrint("\(readyForMoreMediaDataFalseCount): readyForMoreMediaData is false at ii = \(ii)")
                     }
-                    
-                }
-                else {
-                    readyForMoreMediaDataFalseCount += 1
-                    //debugPrint("\(readyForMoreMediaDataFalseCount): readyForMoreMediaData is false at ii = \(ii)")
-                }
-            })
+                })
+            }
         }
+        
 
         return tempPath
         
     }
 
     
-    //func pixelBufferFromCGImage(image: CGImage, size: CGSize) -> UnsafeMutablePointer<CVPixelBuffer?> {
-    func pixelBufferFromCGImage(image: UIImage, pxbuffer: UnsafeMutablePointer<CVPixelBuffer?>) {
+    func pixelBufferFromCGImage(image: UIImage) -> UnsafeMutablePointer<CVPixelBuffer?> {
+    //func pixelBufferFromCGImage(image: UIImage, pxbuffer: UnsafeMutablePointer<CVPixelBuffer?>) {
         
-        // UIImage를 쉽게 CGImage로 바꿀 수 없다는 것을 알았다. 아래의 과정을 거쳐야만 한다. 검은 화면이 나오는 것도 다 이런 이유였다.
-        let ciimage = CIImage(image: image)
-        let cgimage = convertCIImageToCGImage(ciimage!)
+            // UIImage를 쉽게 CGImage로 바꿀 수 없다는 것을 알았다. 아래의 과정을 거쳐야만 한다. 검은 화면이 나오는 것도 다 이런 이유였다.
+            let ciimage = CIImage(image: image)
+            let cgimage = convertCIImageToCGImage(ciimage!)
+            
+            // 아래 주석문의 Objective-C 구문을 swift로 변경하기 위해 이렇게 기나긴 코드를 작성해야 하다니!!
+            // 오죽하면 아래 코드의 원작자도 stupid라는 주석을 달아놓았다!! ㅋㅋ
+            /*
+             NSDictionary *options = @{(id)kCVPixelBufferCGImageCompatibilityKey: @YES,
+             (id)kCVPixelBufferCGBitmapContextCompatibilityKey: @YES};
+             */
+            // stupid CFDictionary stuff
+            let keys: [CFStringRef] = [kCVPixelBufferCGImageCompatibilityKey, kCVPixelBufferCGBitmapContextCompatibilityKey]
+            let values: [CFTypeRef] = [kCFBooleanTrue, kCFBooleanTrue]
+            let keysPointer = UnsafeMutablePointer<UnsafePointer<Void>>.alloc(1)
+            let valuesPointer =  UnsafeMutablePointer<UnsafePointer<Void>>.alloc(1)
+            keysPointer.initialize(keys)
+            valuesPointer.initialize(values)
+            //let options = CFDictionaryCreate(kCFAllocatorDefault, keysPointer, valuesPointer, keys.count, UnsafePointer<CFDictionaryKeyCallBacks>(), UnsafePointer<CFDictionaryValueCallBacks>())
+            // 원래 위 코드였는데 swift3에서 변경되었다고 한다.
+            let options = CFDictionaryCreate(kCFAllocatorDefault, keysPointer, valuesPointer, keys.count, nil, nil)
+            // 여기까지가 CFDictionary를 위한 코드
+            
+            let width = CGImageGetWidth(cgimage)
+            let height = CGImageGetHeight(cgimage)
+            
+            let pxbuffer = UnsafeMutablePointer<CVPixelBuffer?>.alloc(1)
+            // pxbuffer = nil 할 경우 status = -6661 에러 발생한다.
+            var status = CVPixelBufferCreate(kCFAllocatorDefault, width, height,
+                kCVPixelFormatType_32ARGB, options, pxbuffer)
+            if (status != 0){
+                debugPrint("CVPixelBufferCreate status = \(status)")
+            }
+            
+            status = CVPixelBufferLockBaseAddress(pxbuffer.memory!, 0);
+            if (status != 0){
+                debugPrint("CVPixelBufferLockBaseAddress status = \(status)")
+            }
+            
+            let bufferAddress = CVPixelBufferGetBaseAddress(pxbuffer.memory!);
+            //debugPrint("pxbuffer.memory = \(pxbuffer.memory)")
+            
+            
+            let rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+            let bytesperrow = CVPixelBufferGetBytesPerRow(pxbuffer.memory!)
+            let context = CGBitmapContextCreate(bufferAddress, width,
+                height, 8, bytesperrow, rgbColorSpace,
+                CGImageAlphaInfo.NoneSkipFirst.rawValue);
+            //debugPrint("image = \(image)")
+            CGContextDrawImage(context, CGRectMake(0, 0, CGFloat(width), CGFloat(height)), cgimage);
+            
+            status = CVPixelBufferUnlockBaseAddress(pxbuffer.memory!, 0);
+            if (status != 0){
+                debugPrint("CVPixelBufferUnlockBaseAddress status = \(status)")
+            }
 
-        // 아래 주석문의 Objective-C 구문을 swift로 변경하기 위해 이렇게 기나긴 코드를 작성해야 하다니!!
-        // 오죽하면 아래 코드의 원작자도 stupid라는 주석을 달아놓았다!! ㅋㅋ
-        /*
-         NSDictionary *options = @{(id)kCVPixelBufferCGImageCompatibilityKey: @YES,
-         (id)kCVPixelBufferCGBitmapContextCompatibilityKey: @YES};
-        */
-        // stupid CFDictionary stuff
-        let keys: [CFStringRef] = [kCVPixelBufferCGImageCompatibilityKey, kCVPixelBufferCGBitmapContextCompatibilityKey]
-        let values: [CFTypeRef] = [kCFBooleanTrue, kCFBooleanTrue]
-        let keysPointer = UnsafeMutablePointer<UnsafePointer<Void>>.alloc(1)
-        let valuesPointer =  UnsafeMutablePointer<UnsafePointer<Void>>.alloc(1)
-        keysPointer.initialize(keys)
-        valuesPointer.initialize(values)
-        //let options = CFDictionaryCreate(kCFAllocatorDefault, keysPointer, valuesPointer, keys.count, UnsafePointer<CFDictionaryKeyCallBacks>(), UnsafePointer<CFDictionaryValueCallBacks>())
-        // 원래 위 코드였는데 swift3에서 변경되었다고 한다.
-        let options = CFDictionaryCreate(kCFAllocatorDefault, keysPointer, valuesPointer, keys.count, nil, nil)
-        // 여기까지가 CFDictionary를 위한 코드
-
-        let width = CGImageGetWidth(cgimage)
-        let height = CGImageGetHeight(cgimage)
-        
-        //let pxbuffer = UnsafeMutablePointer<CVPixelBuffer?>.alloc(1)
-        // pxbuffer = nil 할 경우 status = -6661 에러 발생한다.
-        var status = CVPixelBufferCreate(kCFAllocatorDefault, width, height,
-                                         kCVPixelFormatType_32ARGB, options, pxbuffer)
-        if (status != 0){
-            debugPrint("CVPixelBufferCreate status = \(status)")
-        }
-        
-        status = CVPixelBufferLockBaseAddress(pxbuffer.memory!, 0);
-        if (status != 0){
-            debugPrint("CVPixelBufferLockBaseAddress status = \(status)")
-        }
-        
-        let bufferAddress = CVPixelBufferGetBaseAddress(pxbuffer.memory!);
-        //debugPrint("pxbuffer.memory = \(pxbuffer.memory)")
-
-        
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB();
-        let bytesperrow = CVPixelBufferGetBytesPerRow(pxbuffer.memory!)
-        let context = CGBitmapContextCreate(bufferAddress, width,
-                                            height, 8, bytesperrow, rgbColorSpace,
-                                            CGImageAlphaInfo.NoneSkipFirst.rawValue);
-        //debugPrint("image = \(image)")
-        CGContextDrawImage(context, CGRectMake(0, 0, CGFloat(width), CGFloat(height)), cgimage);
-
-        status = CVPixelBufferUnlockBaseAddress(pxbuffer.memory!, 0);
-        if (status != 0){
-            debugPrint("CVPixelBufferUnlockBaseAddress status = \(status)")
-        }
+        return pxbuffer
     }
     
     func convertCIImageToCGImage(inputImage: CIImage) -> CGImage! {
